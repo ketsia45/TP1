d8437e61d36503b60e631d732453e3bb2b843245
TP1 <promessebossoukissa@icloud.com>
2023-05-06 13:08:09 +0200

    text tp

---
+++ somme.py
@@ -0,0 +1,9 @@
+# trouver la somme de deux nombres entiers
+nbr1 = input('Entrez le premier nombre: ')
+nbr2 = input('Entrez le deuxieme nombre: ')
+
+# Additionner les deux nombres
+s= int(nbr1) + int(nbr2)
+
+#Afficher la somme
+print('la somme de{0} et {1} est {2}'.Format(nbr1, nbr2, s))
